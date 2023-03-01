17cf3f20a68642e4057753f29ff8d39a723cead0
mohamuhsin <muhsinohoho@outlook.com>
2023-03-01 14:28:51 +0300

    Placed all the components together to form the UI of the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import "./App.css";
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return <h2>Getting started</h2>;
+export default class App extends React.Component {
+render() {
+  return (
+    <div className="App">
+  <h1>ravenous</h1>
+  <SearchBar />
+  <BusinessList />
+</div>
+  );
+}
 }
 
-export default App;
