450ff4dd7c5a888ffcecdfef97ebaa9921666010
mohamuhsin <muhsinohoho@outlook.com>
2023-03-04 01:05:23 +0300

    set state to the SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,42 @@
 import React from "react";
 import "./SearchBar.css";
 
+
 const sortByOptions = {
   "Best Match": "best_match",
   "Highest Rated": "rating",
-  "Most Reviewed": "review_count",
+  "Most Reviewed": "review_count"
 };
 
+
+
 class SearchBar extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+
+
+    }
+  }
+
+  getSortByClass(sortByOption){
+    if(sortByOption === this.sortBy) {
+      return 'active';
+    } else {
+      return '';
+    }
+  }
+
+  handleSortByChange(sortByOption){
+    this.setState({sortBy: sortByOption});
+
+  }
+
+ 
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
