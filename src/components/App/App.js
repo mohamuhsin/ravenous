c1d0bb2b39044107070ef847137a21f86f5fdc86
mohamuhsin <muhsinohoho@outlook.com>
2023-03-04 01:38:25 +0300

    Set the searchYelp Prop in SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,7 +30,7 @@ render() {
   return (
     <div className="App">
   <h1>ravenous</h1>
-  <SearchBar />
+  <SearchBar searchYelp={this.searchYelp} />
   <BusinessList businesses={businesses}/>
 </div>
   );
