5c7f80a7550c2c63cc8549717b23268ce41fb8a7
mohamuhsin <muhsinohoho@outlook.com>
2023-03-09 22:28:11 +0300

    chained .then

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,13 @@ const apiKey = '';
 
 const Yelp = {
     searchYelp(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}` 
+              }
+        }).then(response => {
+            return response.json();
+        })
     }
 
 }
