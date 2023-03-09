097aaf66bdabe9db1e4b7b79e0712d2be202ea3b
mohamuhsin <muhsinohoho@outlook.com>
2023-03-09 22:17:14 +0300

    retrived businesses

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,5 +6,5 @@ const Yelp = {
 }
 
 search(term, location, sortBy) {
-    return fetch();
+    return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&${location}=LOCATION&sort_by=${sortBy}`);
 }
