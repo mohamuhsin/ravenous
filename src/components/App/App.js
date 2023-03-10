2bbc6abcdc131b80d380c052b9c81e9fede7ccaa
mohamuhsin <muhsinohoho@outlook.com>
2023-03-10 18:27:36 +0300

    imported yelp api to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./App.css";
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
 const business = {
   imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
