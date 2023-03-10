33a4eccc72059e0f49c6774435ca7e2e944559cd
mohamuhsin <muhsinohoho@outlook.com>
2023-03-10 19:51:57 +0300

    modified business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,33 +1,29 @@
-import React from "react";
+import React from 'react';
 import './Business.css';
-
-
 
 class Business extends React.Component {
   render() {
     return (
-      <div class="Business">
-        <div class="image-container">
-          <img
-            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
-            alt=""
-          />
+      <div className="Business">
+        <div className="image-container">
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
-        <div class="Business-information">
-          <div class="Business-address">
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+        <h2>{this.props.business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
           </div>
-          <div class="Business-reviews">
-            <h3>ITALIAN</h3>
-            <h3 class="rating">4.5 stars</h3>
-            <p>90 reviews</p>
+          <div className="Business-reviews">
+            <h3>{this.props.business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+            <p>{`${this.props.business.reviewCount} reviews`}</p>
           </div>
         </div>
       </div>
     );
   }
 }
+
 export default Business;
