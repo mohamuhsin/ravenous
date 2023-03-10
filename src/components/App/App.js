ac3ace0209b802e11d66f0a9e349afc2b63bf77d
mohamuhsin <muhsinohoho@outlook.com>
2023-03-10 18:29:55 +0300

    removed hard coded business and businesses from App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,7 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
-const business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Flavortown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
 
-const businesses = [
-  business, business, business, business, business, business
-]
 
 export default class App extends React.Component {
 
